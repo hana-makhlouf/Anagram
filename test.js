afca647ba52d2be9530f53a62511a9c801ecb7ff
Daryl Cecile <6310278+daryl-cecile@users.noreply.github.com>
2019-06-22 18:31:54 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,25 @@
+/**
+ * DO NOT REMOVE THIS COMMENT
+ *
+ * @Target anagram-checker.js
+ */
+
+describe('Words that are anagrams should return true', ()=>{
+	expect(checkAnagram('listen','silent')).toBeTruthy();
+});
+
+describe('Words that are the same length but not anagrams should return false', ()=>{
+	expect(checkAnagram('radium','madrid')).not.toBeTruthy();
+});
+
+describe('Sentences that are anagrams should return true', ()=>{
+	expect(checkAnagram('eleven plus two','twelve plus one')).toBeTruthy();
+});
+
+describe('Same words are not anagrams and should return false', ()=>{
+	expect(checkAnagram('spain','spain')).not.toBeTruthy();
+});
+
+describe('Anagrams can contain spaces and should return true', ()=>{
+	expect(checkAnagram('funeral','real fun')).toBeTruthy();
+});
