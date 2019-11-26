9e1543758988d58f82c15cfb26f8f845889822f4
Hana <hanamakhlouf1@gmail.com>
2019-11-26 19:14:42 +0000

    working anagram code

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,3 +1,24 @@
 
 // More Information about this assisngment is available in the README.md file
 
+/*- Create a function called `checkAnagram` that takes in 2 parameters; the source text, and the anagram text
+- The function should return true if they are anagrams (same length and same letters) and return false if not
+- If the two words are the same, they should not be considered anagrams
+- The function should return a boolean value as soon as possible*/
+
+const checkAnagram = (source, anagram)=>{
+     //source = 'map';
+    source = source.split('').sort().join('');
+
+    // anagram = 'pam';
+    anagram = anagram.split('').sort().join('');
+
+    //test if they are the same word === and return false
+    if(source===anagram){
+        return true;
+    }
+    return false;
+}
+
+
+console.log(checkAnagram('map', 'amp'));
